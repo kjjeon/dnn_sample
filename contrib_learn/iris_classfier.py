2944927ea0a71226732f4710c35a3224184a4032
kookjin jeon <kjjeon@aview.co.kr>
2017-08-16 11:11:35 +0900

    iris monitor 추가

--- contrib_learn/iris_classfier.py
+++ contrib_learn/iris_classfier.py
@@ -30,21 +30,21 @@ def main():
     # Load datasets.
     training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
         filename=IRIS_TRAINING,
-        target_dtype=np.int,
-        features_dtype=np.float32)
+        target_dtype=np.int, # 타겟(정답)의 데이터형
+        features_dtype=np.float32) # 특성의 데이터형
     test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
         filename=IRIS_TEST,
         target_dtype=np.int,
         features_dtype=np.float32)
 
     # Specify that all features have real-value data
+    # 연속적인 컬럼 데이터 설정 (꽃받침 길이,꽃받침 너비, 꽃잎 길이, 꽃잎 너비)
     feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
-
     # Build 3 layer DNN with 10, 20, 10 units respectively.
     classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                                 hidden_units=[10, 20, 10],
-                                                n_classes=3,
-                                                model_dir="/tmp/iris_model")
+                                                n_classes=3, # 타겟 클래스 노드 갯수
+                                                model_dir="/tmp/iris_model") # 훈련 중 체크포인터를 저장할 디렉토리
     # Define the training inputs
     def get_train_inputs():
         x = tf.constant(training_set.data)
@@ -52,7 +52,7 @@ def main():
 
         return x, y
 
-    # Fit model.
+    # 학습 시키기
     classifier.fit(input_fn=get_train_inputs, steps=2000)
 
     # Define the test inputs
@@ -62,13 +62,13 @@ def main():
 
         return x, y
 
-    # Evaluate accuracy.
+    # 모델의 정확도 판단하기
     accuracy_score = classifier.evaluate(input_fn=get_test_inputs,
                                          steps=1)["accuracy"]
 
     print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
 
-    # Classify two new flower samples.
+    # 새로운 두 꽃의 표본을 분류합니다.
     def new_samples():
         return np.array(
             [[6.4, 3.2, 4.5, 1.5],
